9124459e8f18d1da68449ff6c47ac205e877785d
Bryan Moreira <ibryyan@hotmail.com>
2025-02-01 00:23:11 -0300

    HEY-9: it should have at least 10 characters

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -10,7 +10,7 @@ class QuestionController extends Controller
     public function store(): RedirectResponse
     {
         $validatedData = request()->validate([
-            'question' => ['required'],
+            'question' => ['required', 'min:10'],
         ]);
 
         Question::query()->create($validatedData);
