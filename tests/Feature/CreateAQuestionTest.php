9862ea2de8a4a9a07a528b6f7cffea12ac4fc85f
Bryan Moreira <ibryyan@hotmail.com>
2025-02-01 00:42:38 -0300

    HEY-9: it should check if ends with question mark ?

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -23,7 +23,20 @@ it('should be able to create a new question bigger than 255 characters', functio
 });
 
 it('should check if ends with question mark ?', function () {
+    // Arrange :: preparar
+    $user = User::factory()->create();
+    actingAs($user);
 
+    //Act
+    $request = post(route('question.store'), [
+        'question' => str_repeat('a', 10),
+    ]);
+
+    //Assert
+    $request->assertSessionHasErrors([
+        'question' => 'Are you sure that is a question? It is missing the question mark.',
+    ]);
+    assertDatabaseCount('questions', 0);
 });
 
 it('should have at least 10 characters', function () {
